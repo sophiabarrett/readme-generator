aa253e39e90599c2a918d70aa20b6b05f3f56450
Sophia Barrett <sophia@yoursummit.media>
2021-11-06 14:35:35 -0700

    refactor for legibility and fix spelling error

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser().then(answers => {
+    promptUser()
+    .then(answers => {
         console.log(answers);
     })
 }
