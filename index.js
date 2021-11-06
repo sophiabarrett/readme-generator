34b165cde466eaea39aafa2242f4f584a292fab9
Sophia Barrett <sophia@yoursummit.media>
2021-11-06 14:33:09 -0700

    setup questions array

--- index.js
+++ index.js
@@ -6,7 +6,53 @@ const questions = [
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your full name?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project\'s title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Provide installation instrucitons for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information for your project.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution guidelines for your project.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions for your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license for your project.',
+        choices: ['License 1', 'License 2', 'License 3', 'License 4', 'License 5']
     }
 ];
 
