029e0b23aac9e6c1c9af76b83080ab0353d62182
Sophia Barrett <sophia@yoursummit.media>
2021-11-06 13:33:50 -0700

    import inquirer and setup promptUser function

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+];
+
+// Prompt user to answer questions
+function promptUser() {
+    return inquirer
+        .prompt(questions);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser().then(answers => {
+        console.log(answers);
+    })
+}
 
 // Function call to initialize app
 init();
