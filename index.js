acabd4f8b3e35ef9cab0c4462dd2e69dc1045a8f
Sophia Barrett <sophia@yoursummit.media>
2021-11-06 15:01:11 -0700

    pass user input to writeToFile function and setup new Promise

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your full name?'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?'
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is your project\'s title?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of your project.'
-    },
-    {
-        type: 'input',
-        name: 'install',
-        message: 'Provide installation instrucitons for your project.'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Provide usage information for your project.'
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Provide contribution guidelines for your project.'
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Provide test instructions for your project.'
-    },
+    // {
+    //     type: 'input',
+    //     name: 'name',
+    //     message: 'What is your full name?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'github',
+    //     message: 'What is your GitHub username?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'email',
+    //     message: 'What is your email address?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'title',
+    //     message: 'What is your project\'s title?'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'description',
+    //     message: 'Provide a description of your project.'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'install',
+    //     message: 'Provide installation instrucitons for your project.'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'usage',
+    //     message: 'Provide usage information for your project.'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'contribution',
+    //     message: 'Provide contribution guidelines for your project.'
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'test',
+    //     message: 'Provide test instructions for your project.'
+    // },
     {
         type: 'list',
         name: 'license',
@@ -63,13 +65,44 @@ function promptUser() {
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve('Success!');
+        });
+    });
+}
+
+// MOCK DATA FOR DEVELOPMENT
+const mockData = {
+    name: 'Sophie barrett',
+    github: 'sophiabarrett',
+    email: 'sophia@yoursummit.media',
+    title: 'Professional README Generator',
+    description: 'Command line app that generates a professional README file from user inputs.',
+    install: 'Download to local machine and install packages.',
+    usage: 'Anyone can use to make a quick README file.',
+    contribution: 'Anyone can contribute!',
+    test: 'For testing, download and install packages',
+    license: 'License 3'
+  };
 
 // TODO: Create a function to initialize app
 function init() {
     promptUser()
     .then(answers => {
-        console.log(answers);
+        //return generateMarkdown(answers);
+        return generateMarkdown(mockData);
+    })
+    .then(markdown => {
+        return writeToFile('./dist/README.md', markdown);
+    }).then(writeToFileResponse => {
+        console.log(writeToFileResponse);
     })
 }
 
